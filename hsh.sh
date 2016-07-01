e101da896e8c13eb2d8e34291c1bcbfbfc19520c
xhpwn <saxena20@purdue.edu>
2016-07-01 10:23:36 -0400

    copy script hsh.sh

---
+++ hsh.sh
@@ -0,0 +1,18 @@
+#!usr/bin/env bash
+
+echo "================================="
+echo "Anubhav Saxena | saxena.xyz"
+echo "http://github.com/xhpwn/hsh.git"
+echo "================================="
+echo ""
+echo ""
+
+echo "Copying .vimrc..."
+cp .vimrc ~
+echo ".vimrc copied."
+echo ""
+
+echo "Copying .bashrc..."
+cp .bashrc ~
+echo ".bashrc copied."
+echo ""
